62593bb587c4a4b560e9079d2a82512552985666
churly <nietopetinalcarlos@gmail.com>
2023-04-26 20:31:13 +0200

    Add new geoip table

--- mysql/monitor.sql
+++ mysql/monitor.sql
@@ -1,3 +1,27 @@
+
+
+DROP TABLE IF EXISTS `geoip`;
+CREATE TABLE `geoip` (
+  `ip` varchar(15) NOT NULL,
+  `nomorg` varchar(100) DEFAULT NULL,
+  `numorg` varchar(20) DEFAULT NULL,
+  `postal_code` varchar(20) DEFAULT NULL,
+  `city_name` varchar(50) DEFAULT NULL,
+  `country_name` varchar(50) DEFAULT NULL,
+  `country_iso_code` varchar(20) DEFAULT NULL,
+  `region_name` varchar(100) DEFAULT NULL,
+  `region_iso_code` varchar(20) DEFAULT NULL,
+  `continent_code` varchar(20) DEFAULT NULL,
+  `timezone` varchar(50) DEFAULT NULL,
+  `lon` double DEFAULT NULL,
+  `lat` double DEFAULT NULL,
+  `ipc` int(11) DEFAULT NULL,
+  `fechreg` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`ip`)
+) ENGINE=InnoDB;
+
+
+
 -- kafka.monitor DDL
 
 DROP TABLE IF EXISTS `monitor`;
