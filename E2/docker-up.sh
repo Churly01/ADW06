b1d11f112c1bca0414c50d10212c862cc001a105
Willy19921992 <guuillekarateka@gmail.com>
2023-05-14 13:12:07 +0200

    Separar en varios archivos

--- E2/docker-up.sh
+++ E2/docker-up.sh
@@ -9,7 +9,16 @@ read -n 1 -p "Press key after logstash started"
 
 sleep 2
 read -n 1 -p "Press key after ksqldb-server started"
-docker exec -t ksqldb-cli ksql -f /server.ksql http://ksqldb-server:8088
-sleep 1
-#Haha no esperar a que se creen los topics goes brrrrrrrr
-docker exec -t ksqldb-cli ksql -f /server.ksql http://ksqldb-server:8088
+docker exec -t ksqldb-cli ksql -f /apache_raw.ksql http://ksqldb-server:8088
+docker exec -t ksqldb-cli ksql -f /connector_monitor.ksql http://ksqldb-server:8088
+docker exec -t ksqldb-cli ksql -f /stream_monitor.ksql http://ksqldb-server:8088
+docker exec -t ksqldb-cli ksql -f /stream_acceso.ksql http://ksqldb-server:8088
+docker exec -t ksqldb-cli ksql -f /table_nacc.ksql http://ksqldb-server:8088
+docker exec -t ksqldb-cli ksql -f /table_geoip.ksql http://ksqldb-server:8088
+docker exec -t ksqldb-cli ksql -f /connector_geoip.ksql http://ksqldb-server:8088
+
+read -n 1 -p "Press to launch queries"
+trap '' 2
+docker exec -t ksqldb-cli ksql -f /q1.ksql http://ksqldb-server:8088
+docker exec -t ksqldb-cli ksql -f /q2.ksql http://ksqldb-server:8088
+trap 2
